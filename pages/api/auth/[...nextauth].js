daad67503215bc5db7fe97e19bb7b2cf2d8d3c0c
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-10-29 10:17:03 +0530

    Added another email'

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
-const adminEmails = ['vy122427@gmail.com'];
+const adminEmails = ['vy122427@gmail.com', 'kirankumari01536@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
